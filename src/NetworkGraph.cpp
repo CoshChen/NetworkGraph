eb1788b0a26df6c28a3dc08f018dcd4b909b8b5d
CoshChen <cosh.chen@gmail.com>
2017-10-31 18:19:01 -0400

    Create NetworkGraph.cpp

---
+++ src/NetworkGraph.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : NetworkGraph.cpp
+// Author      : Ko-Shin Chen
+// Description : Sample program in C++
+//============================================================================
+
+#include "Test.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Start test!" << endl;
+	test_1();
+	cout << "End test!" << endl;
+
+	return 0;
+}
